62d514e6960a0662a031af5064167512c158124b
DrakeVConaway <dc7012@ship.edu>
2018-10-29 14:43:38 -0400

    Added incomplete wc/wr files

---
+++ core/src/gameWorld/WorldController.java
@@ -0,0 +1,30 @@
+package gameWorld;
+/**
+ * Keeps track of objects within the world, handles updates
+ * Makes sure the renderer is able to draw objects
+ * after they have updated
+ * @author Drake Conaway
+ */
+public class WorldController {
+
+	private static final String TAG =
+			WorldController.class.getName();
+	/**
+	 * World Controller constructor
+	 */
+	public WorldController() {}
+	
+	/**
+	 * Initialization method for 
+	 * WorldController
+	 */
+	public void init() {}
+	
+	/**
+	 * Update method for WorldController
+	 * used to update object's positions
+	 * before rendering, 
+	 * @param deltaTime
+	 */
+	public void update(float deltaTime) {}
+}
---
+++ core/src/utils/Constants.java
@@ -0,0 +1,9 @@
+package utils;
+
+public class Constants {
+//visible game world is 5 meters wide
+	public static final float VIEWPORT_WIDTH = 5.0f;
+	
+	//visible game world is 5 meters tall
+	public static final float VIEWPORT_HEIGHT = 5.0f;
+}
