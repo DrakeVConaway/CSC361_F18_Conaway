638c26cad7b77bfa7ff1b9278c44c8379995e5c0
Drake Conaway <dc7012@ship.edu>
2018-11-17 20:25:52 -0500

    Commit

--- core/src/gameObjects/Goal.java
+++ core/src/gameObjects/Goal.java
@@ -14,9 +14,12 @@ public class Goal extends AbstractGameObject {
 	public Goal(){
 		init();
 	}
+<<<<<<< HEAD
 	/**
 	 * Set dimensions, bounds
 	 */
+=======
+>>>>>>> Milestone_3
 	private void init(){
 		dimension.set(3.0f,3.0f);
 		regGoal = Assets.instance.levelDecoration.goal;
@@ -25,11 +28,15 @@ public class Goal extends AbstractGameObject {
 		bounds.set(1,Float.MIN_VALUE,10,Float.MAX_VALUE );
 		origin.set(dimension.x/2.0f,0.0f);
 	}
+<<<<<<< HEAD
 	/**
 	 * Render the Baphomet
 	 * @param once again a 
 	 * SpriteBatch
 	 */
+=======
+	
+>>>>>>> Milestone_3
 	public void render(SpriteBatch batch){
 		TextureRegion reg = regGoal;
 		
